369c351c0e40f4c96914b95c78ce9dcfb6bf9268
Sean Wong <wotzhs@gmail.com>
2020-04-27 04:23:20 +0800

    auth: add missing password or email error constant message in auth error

--- api/src/models/error.js
+++ api/src/models/error.js
@@ -12,6 +12,10 @@ class AuthError extends Error {
 	static get PASSWORD_EMAIL_MISMATCH_MSG() {
 		return "email or password is incorrect";
 	}
+
+	static get MISSING_PASSWORD_OR_EMAIL_MSG() {
+		return "missing email or password";
+	}
 }
 
 class RecordNotFoundError extends Error {
