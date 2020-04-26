286015612f7b9f210afce770f6c01d143ddf9375
Sean Wong <wotzhs@gmail.com>
2020-04-27 04:15:59 +0800

    auth: add get user by email service

--- api/src/services/users.js
+++ api/src/services/users.js
@@ -1,6 +1,7 @@
 import { v4 as uuidV4 } from "uuid";
 import pool from "../db";
 import User from "../models/user";
+import { RecordNotFoundError } from "../models/error";
 
 class UsersService {
 	static async createUser(user) {
@@ -31,6 +32,28 @@ class UsersService {
 		try {
 			const exists = await pool.query("SELECT id from users WHERE email=$1", [ user.email ]);
 			return exists.rowCount > 0;
+		} catch (e) {
+			return e;
+		}
+	}
+
+	static async getUserByEmail(email) {
+		try {
+			const result = await pool.query(
+				`
+				SELECT users.id, users.email, users.password_hash, roles.name role_name
+				FROM users
+				LEFT JOIN roles
+					ON users.role_id = roles.id
+				WHERE users.email = $1
+				`, [ email ]
+			);
+
+			if (result.rowCount < 1) {
+				throw new RecordNotFoundError("user not found");
+			}
+
+			return result.rows[0];
 		} catch (e) {
 			return e;
 		}
