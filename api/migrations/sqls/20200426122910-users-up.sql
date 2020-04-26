e148e0a659f141fe460418ece6547d2c3bbbd487
Sean Wong <wotzhs@gmail.com>
2020-04-26 22:15:15 +0800

    users: add unique constraint to email field

--- api/migrations/sqls/20200426122910-users-up.sql
+++ api/migrations/sqls/20200426122910-users-up.sql
@@ -1,7 +1,7 @@
 /* Replace with your SQL commands */
 CREATE TABLE IF NOT EXISTS users (
 	id UUID PRIMARY KEy,
-	email VARCHAR NOT NULL,
+	email VARCHAR NOT NULL UNIQUE,
 	password_hash VARCHAR NOT NULL,
 	role_id UUID REFERENCES roles(id),
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
