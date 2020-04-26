42524b45cb3db7d9f51f4a9db7d287e3e4438d8e
Sean Wong <wotzhs@gmail.com>
2020-04-27 02:33:48 +0800

    users: complete new user route

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,10 +1,55 @@
 import express from "express";
+import HttpStatus from "http-status-codes";
+import User from "../models/user";
+import Role from "../models/role";
+import RolesService from "../services/roles";
+import UsersService from "../services/users";
+
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function(req, res, next) {
-	res.send("respond with a resources");
+router.post("/new", async (req, res, next) => {
+	const { email, password, user_type } = req.body;
 
+	if (user_type !== Role.RECRUITER && user_type !== Role.CANDIDATE) {
+		res.status(HttpStatus.BAD_REQUEST);
+		return res.json({ error: `user_type must be either '${ Role.RECRUITER }' or '${ Role.CANDIDATE }` });
+	}
+
+	const roleId = user_type === "recruiter" ? await RolesService.getRecruiterRoleId() : await RolesService.getCandidateRoleId();
+	if (roleId instanceof Error) {
+		console.log(roleId);
+		res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+		return res.json({ error: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) });
+	}
+
+	const newUser = new User(email, password, roleId);
+	const errors = newUser.validate();
+	if (errors) {
+		res.status(HttpStatus.BAD_REQUEST);
+		return res.json({ errors: errors });
+	}
+
+	const exists = await UsersService.existsUser(newUser);
+	if (exists instanceof Error) {
+		console.log(exists);
+		res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+		return res.json({ error: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) });
+	}
+
+	if (exists) {
+		res.status(HttpStatus.CONFLICT);
+		return res.json({ error: `an account with the email ${email} has already been registered previously` });
+	}
+
+	const result = await UsersService.createUser(newUser);
+	if (result instanceof Error) {
+		console.log(result);
+		res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+		return res.json({ error: HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR) });
+	}
+
+	res.status(HttpStatus.CREATED);
+	res.send();
 });
 
 export default router;
