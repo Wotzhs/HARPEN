27f3c29358cf444f6f3190d36f97ddb93a47aa3b
Sean Wong <wotzhs@gmail.com>
2020-04-27 02:35:27 +0800

    users: add user and roles services

---
+++ api/src/services/users.js
@@ -0,0 +1,40 @@
+import { v4 as uuidV4 } from "uuid";
+import pool from "../db";
+import User from "../models/user";
+
+class UsersService {
+	static async createUser(user) {
+		if (user instanceof User === false) {
+			return Error("argument user must be an instance of user");
+		}
+
+		const passwordHash = await user.getPasswordHash();
+		if (passwordHash instanceof Error) {
+			return passwordHash;
+		}
+
+		try {
+			await pool.query(
+				"INSERT INTO users (id, email, password_hash, role_id) VALUES ($1, $2, $3, $4)",
+				[ uuidV4(), user.email, passwordHash, user.role_id ],
+			);
+		} catch (e) {
+			return e;
+		}
+	}
+
+	static async existsUser(user) {
+		if (user instanceof User === false) {
+			return Error("argument user must be an instance of user");
+		}
+
+		try {
+			const exists = await pool.query("SELECT id from users WHERE email=$1", [ user.email ]);
+			return exists.rowCount > 0;
+		} catch (e) {
+			return e;
+		}
+	}
+}
+
+export default UsersService;
