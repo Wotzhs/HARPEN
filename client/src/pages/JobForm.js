6e23d3f8ba29bb79f31e9d29cf128b46af05fff5
Sean Wong <wotzhs@gmail.com>
2020-04-28 05:09:46 +0800

    new_job: add basic add new job form

--- client/src/pages/JobForm.js
+++ client/src/pages/JobForm.js
@@ -1,8 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import DatePicker from "react-date-picker";
 
-const JobForm = () => {
+const JobForm = ({ history }) => {
+	const [ formValues, setFormValues ] = useState({});
+	const [ errors, setErrors ] = useState({});
+
+	const handleChange = e => {
+		setFormValues({ ...formValues, [e.target.name]: e.target.value });
+	};
+
+	const handleStatusChange = e => {
+		setFormValues({ ...formValues, [e.target.name]: e.target.checked });
+	};
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			const res = await axios.post("/api/jobs", { ...formValues });
+			history.push(res.data.slug);
+		} catch (e) {
+			setErrors({ ...errors, ...(e.response.data) });
+		}
+	};
+
+	const setPostDate = date => {
+		setFormValues({ ...formValues, posting_date: date });
+	};
+
 	return (
-		<div>job form</div>
+		<form onSubmit={ handleSubmit } className="wider-width auto-margin">
+			<div className="content">
+				<h1 className="has-text-centered">Add a new job</h1>
+			</div>
+
+			<div className="field">
+				<label className="label">Title</label>
+				<div className="control">
+					<input
+						className="input"
+						type="text"
+						placeholder="Senior Mechanic"
+						name="title"
+						value={ formValues.title || "" }
+						onChange={ handleChange }
+					/>
+				</div>
+			</div>
+
+			<div className="field">
+				<label className="label">Location</label>
+				<div className="control">
+					<input
+						className="input"
+						type="text"
+						placeholder="Text input"
+						name="location"
+						value={ formValues.location || "" }
+						onChange={ handleChange }
+					/>
+				</div>
+			</div>
+
+			<div className="field">
+				<label className="label">Description</label>
+				<div className="control">
+					<textarea
+						className="textarea"
+						placeholder="e.g. Need to have 30 years of experience"
+						name="description"
+						value={ formValues.description || "" }
+						onChange={ handleChange }
+					>
+					</textarea>
+				</div>
+			</div>
+
+			<div className="field is-grouped">
+				<div className="control">
+					<div className="field is-grouped flex-align-baseline">
+						<label className="has-small-right-margin">Post Date</label>
+						<DatePicker
+							name="date"
+							onChange={ setPostDate }
+							value={ formValues.posting_date || new Date() }
+							format="dd-MM-y"
+						/>
+					</div>
+				</div>
+				<div className="field is-flex flex-align-baseline">
+					<input
+						id="status"
+						type="checkbox"
+						name="status"
+						className="switch is-rtl"
+						onChange={ handleStatusChange }
+						defaultChecked
+					/>
+					<label htmlFor="status">Is job active?</label>
+				</div>
+			</div>
+
+
+
+			<div className="field is-grouped">
+				<div className="control">
+					<button className="button is-primary">Add Job</button>
+				</div>
+				<div className="control">
+					<button
+						onClick={ () => history.goBack() }
+						className="button is-link is-light"
+						type="button"
+					>
+						Cancel
+					</button>
+				</div>
+			</div>
+		</form>
 	);
 };
 
