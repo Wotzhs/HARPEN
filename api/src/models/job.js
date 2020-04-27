887b6375d8018e6632b2e7ba34eb048d0d3f2b5f
Sean Wong <wotzhs@gmail.com>
2020-04-27 09:38:56 +0800

    job: add basic job model

---
+++ api/src/models/job.js
@@ -0,0 +1,50 @@
+import slugify from "slugify";
+
+class Job {
+	static INCORRECT_INSTANCE_MSG = "argument job not an instance of Job";
+
+	constructor({ id, slug, title, location, description, posting_date, status, jd_file, user_id }) {
+		this.id = id;
+		this.slug = slug;
+		this.title = title;
+		this.location = location;
+		this.description = description;
+		this.posting_date = posting_date;
+		this.status = status;
+		this.jd_file = jd_file;
+		this.user_id = user_id;
+	}
+
+	slugify(counter) {
+		this.slug = slugify(this.title);
+		if (counter) {
+			this.slug += `-${counter}`;
+		}
+	}
+
+	validate() {
+		const error = {};
+		if (!this.title) {
+			error.title = "missing title";
+		}
+		if (!this.location) {
+			error.location = "missing location";
+		}
+		if (!this.description) {
+			error.description = "missing description";
+		}
+		if (!this.posting_date) {
+			error.posting_date = "missing posting_date";
+		}
+		if (!this.status) {
+			error.status = "missing status";
+		}
+		if (!this.user_id) {
+			error.user_id = "missing user_id";
+		}
+
+		return Object.keys(error).length > 0 ? error : null;
+	}
+}
+
+export default Job;
