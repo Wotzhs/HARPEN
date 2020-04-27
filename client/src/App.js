aa19efd9b5f48c32237e473c20f21f77a618628d
Sean Wong <wotzhs@gmail.com>
2020-04-27 22:42:25 +0800

    react: add basic react setup

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Menu from "./components/Menu";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import JobListing from "./pages/JobListing";
+import JobDetails from "./pages/JobDetails";
+import JobForm from "./pages/JobForm";
+import { IdentityProvider } from "./contexts/Identity";
+
+const App = () => {
+	return (
+		<IdentityProvider>
+			<Menu />
+			<div>
+				<Switch>
+					<Route exact path="/signup" component={ Signup } />
+					<Route exact path="/login" component={ Login } />
+					<Route exact path="/" component={ JobListing } />
+					<Route exact path="/:slug" component={ JobDetails } />
+					<Route exact path="/new" component={ JobForm } />
+					<Route exact path="/edit/:slug" component={ JobForm } />
+				</Switch>
+			</div>
+		</IdentityProvider>
+	);
+};
+
+export default App;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+	return (
+		<div className="is-flex full-height center-center">
+			<form action="" className="wide-width">
+				<div className="field">
+					<label className="label">Email</label>
+					<div className="control">
+						<input className="input" type="text" name="email" placeholder="john@doe.com" />
+					</div>
+				</div>
+
+				<div className="field">
+					<label className="label">Password</label>
+					<div className="control">
+						<input className="input" type="password" name="password" placeholder="" />
+					</div>
+				</div>
+
+				<div className="field is-grouped">
+					<div className="control">
+						<button className="button is-link">Login</button>
+					</div>
+					<Link className="button is-link is-light" to={ "/" }>Cancel</Link>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
